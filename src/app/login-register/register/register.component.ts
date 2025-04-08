d7f2b207fdd4b4fa50a45441d8301561ae1735ba
Djindustrie@Protonmail.com <Djindustrie@Protonmail.com>
2025-04-09 01:10:00 +0200

    add register.ts from

--- src/app/login-register/register/register.component.ts
+++ src/app/login-register/register/register.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  fb = inject(FormBuilder);
+  router = inject(Router);
 
+  /**
+   * Reaktives Formular mit den Feldern `Username`, `email` und `password`.
+   * Alle Felder sind als erforderlich markiert (Validators.required).
+   */
+  form = this.fb.nonNullable.group({
+    Username: ['', Validators.required],
+    Email: ['', Validators.required],
+    Password: ['', Validators.required],
+  });
+  errorMessage: string | null = null;
+
+  onSubmit(): void {
+    console.log('register');
+  }
 }
