2997a086a5b267a50e130d2f5c26a8728064d4bd
Djindustrie@Protonmail.com <Djindustrie@Protonmail.com>
2025-04-14 01:09:22 +0200

    Remove environment.firebase.ts from tracking

--- src/app/environment.firebase.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyApf7Vsl1sMuwLTZyHAZ-eIwYXOtMeYJM0",
-  authDomain: "lingual-instinct.firebaseapp.com",
-  projectId: "lingual-instinct",
-  storageBucket: "lingual-instinct.firebasestorage.app",
-  messagingSenderId: "229900336597",
-  appId: "1:229900336597:web:9594dba1ec75787179515c",
-  measurementId: "G-FZ2LH9KLQW"
-  };
