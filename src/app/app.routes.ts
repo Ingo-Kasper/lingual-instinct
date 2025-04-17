90edd9e9c4ccaa0c53853caa981276b92113ea3a
Djindustrie@Protonmail.com <Djindustrie@Protonmail.com>
2025-04-17 17:50:01 +0200

    add main-content Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 export const routes: Routes = [
+    { path: '', component: MainContentComponent},
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent}
 ];
