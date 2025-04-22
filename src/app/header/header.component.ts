e5293e5da7e9972a304065a28dcfeeedc65731c5
Djindustrie@Protonmail.com <Djindustrie@Protonmail.com>
2025-04-22 14:10:14 +0200

    Header add Login divs

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit  } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  authService = inject(AuthService);
 
+  /**
+   * Initialisiert die Komponente und abonniert Benutzerinformationen.
+   * Speichert den Zustand des aktuellen Benutzers in einem Signal.
+   */
+  ngOnInit(): void {
+    this.authService.user$.subscribe((user) => {
+      if (user) {
+        // Updates the user information in the signal
+        this.authService.currentUserSig.set({
+          email: user.email!,
+          username: user.displayName!,
+        });
+      } else {
+        // Resets user information if no user is logged in
+        this.authService.currentUserSig.set(null);
+      }
+    });
+  }
 }
